41b176f19112b7ffc40fc8483bb770a1d781376e
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-22 16:16:10 +0600

    done portfolio

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 
 def homepage(request):
-    if request.method=='POST':
-        name=request.POST['name']
-        email=request.POST['email']
-        message=request.POST['message']
-        data={
-            'name':name,
-            'email':email,
-            'message':message
-        }
-        print(data)
+
     return render(request,'index-2.html')
